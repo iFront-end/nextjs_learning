b9407e7e4509925a12d082a93ce3a2f5df10d8fa
Andrey Krutikov <andrei.krutikov@novardis.com>
2025-03-08 00:30:29 +0300

    #15 commit

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,17 +10,13 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/*<Suspense fallback={<CardsSkeleton />}>
-         <CardWrapper />
-        </Suspense>*/}
+
+
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/*<Suspense fallback={<RevenueChartSkeleton />}>*/}
+
          <RevenueChart revenue={revenue} />
-        {/*</Suspense>*/}
-       {/* <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
-        </Suspense>*/}
+
       </div>
     </main>
   );
